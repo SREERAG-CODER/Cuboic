700cc4d077f05a652fdd01ee93794668b9e5f059
SREERAG <sreeragcollege@gmail.com>
2026-04-08 00:19:24 +0530

    aas

--- thambi_desktop/vite.config.ts
+++ thambi_desktop/vite.config.ts
@@ -12,6 +12,15 @@ export default defineConfig({
     electron({
       main: {
         entry: 'electron/main.ts',
+        vite: {
+          build: {
+            rollupOptions: {
+              output: {
+                codeSplitting: false,
+              },
+            },
+          },
+        },
       },
       preload: {
         input: 'electron/preload.ts',
